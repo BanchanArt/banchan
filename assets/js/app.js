e5ea30b5430bebdccb076891c04466f23261eff6
Kat Marchán <kzm@zkat.tech>
2021-05-08 16:46:22 -0700

    remove all the default garbage

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,8 +22,8 @@ let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToke
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+window.addEventListener("phx:page-loading-start", _ => topbar.show())
+window.addEventListener("phx:page-loading-stop", _ => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
