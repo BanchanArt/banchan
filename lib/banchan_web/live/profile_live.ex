8480a7ab87a03eecda050c09e89e6698988a93fc
Kat Marchán <kzm@zkat.tech>
2021-07-02 16:37:07 -0700

    playing around a bit more with profile page

--- lib/banchan_web/live/profile_live.ex
+++ lib/banchan_web/live/profile_live.ex
@@ -4,23 +4,26 @@ defmodule BanchanWeb.ProfileLive do
   """
   use BanchanWeb, :surface_view
 
-  alias Banchan.Accounts.User
-  alias Banchan.Repo
+  alias Banchan.Accounts
 
   alias BanchanWeb.Components.Layout
 
   @impl true
-  def mount(%{"user" => handle}, session, socket) do
+  def mount(%{"handle" => handle}, session, socket) do
     socket = assign_defaults(session, socket)
-    user = Repo.get_by!(User, handle: handle)
-    {:ok, assign(socket, query: "", results: %{}, user: user)}
+    user = Accounts.get_user_by_handle!(handle)
+    {:ok, assign(socket, user: user)}
   end
 
   @impl true
   def render(assigns) do
     ~F"""
     <Layout current_user={@current_user} flashes={@flash}>
+      {#if @live_action == :index}
       Profile page for {@user}
+      {#else if @live_action == :edit && @user.id == @current_user.id}
+      Editing profile for {@user}
+      {/if}
     </Layout>
     """
   end
