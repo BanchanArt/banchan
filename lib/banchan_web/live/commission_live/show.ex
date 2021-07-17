e9ef654f0b7a6ca56183683d5452ffee8049045a
Kat Marchán <kzm@zkat.tech>
2021-07-17 11:41:02 -0700

    some styling

--- lib/banchan_web/live/commission_live/show.ex
+++ lib/banchan_web/live/commission_live/show.ex
@@ -33,13 +33,13 @@ defmodule BanchanWeb.CommissionLive.Show do
       <h2 class="subtitle"><strong>@{@current_user.handle}</strong> requested this commission 3 days ago.</h2>
       <hr>
       <div class="commission columns">
-        <div class="messages column is-three-quarters">
+        <div class="timeline column is-three-quarters">
           <article class="timeline-item message is-link light">
             <div class="message-header">
               <div class="level">
                 <div class="level-left">
                   <figure class="image is-24x24">
-                    <img class="is-rounded" src={Routes.static_path(Endpoint, "/images/kat-chibi.jpeg")} />
+                    <img class="is-rounded" src={Routes.static_path(Endpoint, "/images/kat-chibi.jpeg")}>
                   </figure>
                   @{@current_user.handle} commented 3 days ago.
                 </div>
@@ -51,26 +51,26 @@ defmodule BanchanWeb.CommissionLive.Show do
               </div>
               <div class="level">
                 <div class="level-left">
-                <figure class="image is-96x96">
-                  <img src={Routes.static_path(Endpoint, "/images/penana-left.png")} />
-                </figure>
-                <figure class="image is-96x96">
-                  <img src={Routes.static_path(Endpoint, "/images/penana-right.png")} />
-                </figure>
-                <figure class="image is-96x96">
-                  <img src={Routes.static_path(Endpoint, "/images/penana-front.png")} />
-                </figure>
+                  <figure class="image is-96x96">
+                    <img src={Routes.static_path(Endpoint, "/images/penana-left.png")}>
+                  </figure>
+                  <figure class="image is-96x96">
+                    <img src={Routes.static_path(Endpoint, "/images/penana-right.png")}>
+                  </figure>
+                  <figure class="image is-96x96">
+                    <img src={Routes.static_path(Endpoint, "/images/penana-front.png")}>
+                  </figure>
                 </div>
               </div>
             </div>
           </article>
-          <p class="timeline-item"><small><i class="fas fa-clipboard-check" /> @{@current_user.handle} submitted this commission 3 days ago.</small></p>
+          <p class="timeline-item"><i class="fas fa-clipboard-check" /> @{@current_user.handle} submitted this commission 3 days ago.</p>
           <article class="timeline-item message is-link light">
             <div class="message-header">
               <div class="level">
                 <div class="level-left">
                   <figure class="image is-24x24">
-                    <img class="is-rounded" src={Routes.static_path(Endpoint, "/images/denizen_default_icon.png")} />
+                    <img class="is-rounded" src={Routes.static_path(Endpoint, "/images/denizen_default_icon.png")}>
                   </figure>
                   @skullbunnygalaxy commented 2 days ago.
                 </div>
@@ -78,19 +78,21 @@ defmodule BanchanWeb.CommissionLive.Show do
             </div>
             <div class="message-body">
               <div class="content">
-                Hi, this sounds good. I can get started as soon as I receive the initial payment!
+                <p>Hi, I'm happy to work on this! It sounds really cute.</p>
+                <p>I can get started as soon as I receive the initial payment!</p>
               </div>
             </div>
           </article>
-          <p class="timeline-item"><small><i class="fas fa-file-invoice-dollar" /> @skullbunnygalaxy requested <strong>$100.25</strong></small></p>
-          <p class="timeline-item"><small><i class="fas fa-donate" /> @{@current_user.handle} paid <strong>$100.25</strong></small></p>
-          <p class="timeline-item"><small><i class="fas fa-palette" /> @skullbunnygalaxy started working on this commission.</small></p>
+          <p class="timeline-item"><i class="fas fa-clipboard-check" /> @skullbunnygalaxy accepted this commission 2 days ago.</p>
+          <p class="timeline-item"><i class="fas fa-file-invoice-dollar" /> @skullbunnygalaxy requested <strong>$100.25</strong></p>
+          <p class="timeline-item"><i class="fas fa-donate" /> @{@current_user.handle} paid <strong>$100.25</strong></p>
+          <p class="timeline-item"><i class="fas fa-palette" /> @skullbunnygalaxy started working on this commission.</p>
           <article class="timeline-item message is-link light">
             <div class="message-header">
               <div class="level">
                 <div class="level-left">
                   <figure class="image is-24x24">
-                    <img class="is-rounded" src={Routes.static_path(Endpoint, "/images/denizen_default_icon.png")} />
+                    <img class="is-rounded" src={Routes.static_path(Endpoint, "/images/denizen_default_icon.png")}>
                   </figure>
                   @skullbunnygalaxy commented 2 days ago.
                 </div>
@@ -102,6 +104,7 @@ defmodule BanchanWeb.CommissionLive.Show do
               </div>
             </div>
           </article>
+          <p class="timeline-item"><small><i class="fas fa-hourglass-half" /> @skullbunnygalaxy changed the status to Waiting for Customer.</small></p>
           <hr />
           <Form for={@changeset}>
             <Field class="field" name={:message}>
@@ -142,34 +145,35 @@ defmodule BanchanWeb.CommissionLive.Show do
               </li>
             </ul>
           </div>
-          <hr />
+          <hr>
           <div class="sidebar-box content">
-            <p><strong>Estimate</strong></p>
-            <p><i class="fas fa-calculator"></i> $250.00</p>
+            <p><strong>Status</strong></p>
+            <p><i class="fas fa-hourglass-half" /> Waiting for Customer</p>
           </div>
-          <hr />
+          <hr>
+          <div class="sidebar-box content">
+            <p><strong>Estimated</strong></p>
+            <p><i class="fas fa-calculator" /> $250.00</p>
+          </div>
+          <hr>
           <div class="sidebar-box content">
             <p><strong>Paid</strong></p>
-            <p><i class="fas fa-donate"></i> $100.25</p>
+            <p><i class="fas fa-donate" /> $100.25</p>
           </div>
-          <hr />
-          <div class="sidebar-box content">
-            <p><strong>Remaining</strong></p>
-            <p><i class="fas fa-file-invoice-dollar"></i> $149.75</p>
-          </div>
+          <hr>
           <div class="sidebar-box content">
             <p><strong>Attachments</strong></p>
             <div class="level">
               <div class="level-left">
-              <figure class="image is-96x96">
-                <img src={Routes.static_path(Endpoint, "/images/penana-left.png")} />
-              </figure>
-              <figure class="image is-96x96">
-                <img src={Routes.static_path(Endpoint, "/images/penana-right.png")} />
-              </figure>
-              <figure class="image is-96x96">
-                <img src={Routes.static_path(Endpoint, "/images/penana-front.png")} />
-              </figure>
+                <figure class="image is-96x96">
+                  <img src={Routes.static_path(Endpoint, "/images/penana-left.png")}>
+                </figure>
+                <figure class="image is-96x96">
+                  <img src={Routes.static_path(Endpoint, "/images/penana-right.png")}>
+                </figure>
+                <figure class="image is-96x96">
+                  <img src={Routes.static_path(Endpoint, "/images/penana-front.png")}>
+                </figure>
               </div>
             </div>
           </div>
