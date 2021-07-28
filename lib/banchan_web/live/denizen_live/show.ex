eff916ca2773400c6fadba2b7ec5e24dc6de28b6
Atrian <411004+lunitaire@users.noreply.github.com>
2021-07-28 17:24:01 -0400

    Added a follow button and fixed issues with edit button and profile layout.

--- lib/banchan_web/live/denizen_live/show.ex
+++ lib/banchan_web/live/denizen_live/show.ex
@@ -24,11 +24,26 @@ defmodule BanchanWeb.DenizenLive.Show do
     ~F"""
     <Layout current_user={@current_user} flashes={@flash}>
       <section class="hero is-primary">
+        <div class="hero-head">
+
+        </div>
         <div class="hero-body">
-          <h1 class = "title"> {@user.handle} </h1>
-          <p class = "subtitle"> Witty phrase here. </p>
+          <article class="media">
+            <figure class="image is-48x48 media-left">
+              <img src={Routes.static_path(Endpoint, "/images/denizen_default_icon.png")}>
+            </figure>
+            <div class="media-content">
+              <h1 class = "title"> {@user.handle} </h1>
+              <p class = "subtitle"> Witty phrase here. </p>
+            </div>
+            <div class="media-right">
+              <a class="button is-warning">Follow</a>
+            </div>
+          </article>
+
         </div>
         <div class="hero-foot">
+
           <nav class="tabs is-boxed is-right">
             <div class="container">
               <ul>
@@ -36,7 +51,6 @@ defmodule BanchanWeb.DenizenLive.Show do
                   <a> Profile Home </a>
                 </li>
                 <li> <a>Featured</a> </li>
-                <li> <a>Showcases</a> </li>
                 <li> <a>Characters</a> </li>
               </ul>
             </div>
@@ -48,7 +62,7 @@ defmodule BanchanWeb.DenizenLive.Show do
         <div class="tile is-parent is-12">
           <div class="tile is-child notification is-success">
             <h2 class="title">Studios</h2>
-            <div class="columns denizen-studios is-multiline">
+            <div class="columns denizen-studios is-multiline block">
               {#for studio <- @studios}
                 <div class="column">
                   <StudioCard studio={studio} />
@@ -56,15 +70,18 @@ defmodule BanchanWeb.DenizenLive.Show do
               {/for}
             </div>
           </div>
-          <div class="tile is-child notification is-warning">
-            <p class="title"> About {@user.handle} </p>
-            <p>Name: {@user.name}</p>
-            <p>Bio: {@user.bio}</p>
-            <button>
-              {#if @current_user && @current_user.id == @user.id}
-                <LiveRedirect label="Edit" to={Routes.denizen_edit_path(Endpoint, :edit, @user.handle)} />
-              {/if}
-            </button>
+          <div class="tile is-child notification is-warning block">
+            <h2 class="title"> About {@user.handle} </h2>
+            <figure class="image is-3by1" alt="denizen ID">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+            <div class="content">
+              <p class="">Name: {@user.name}</p>
+              <p class="">Bio: {@user.bio}</p>
+                {#if @current_user && @current_user.id == @user.id}
+                  <LiveRedirect label="Edit" to={Routes.denizen_edit_path(Endpoint, :edit, @user.handle)} class="button is-primary is-fullwidth"/>
+                {/if}
+            </div>
           </div>
 
         </div>
