08ecf573f74cba38e92867b05beac1444722cbcb
Atrian Wagner <411004+lunitaire@users.noreply.github.com>
2021-08-06 17:48:34 -0400

    Profile Update (#42)
    * Implemented tiles to profile page.
    
    * Added a follow button and fixed issues with edit button and profile layout.

--- lib/banchan_web/live/denizen_live/show.ex
+++ lib/banchan_web/live/denizen_live/show.ex
@@ -23,22 +23,75 @@ defmodule BanchanWeb.DenizenLive.Show do
   def render(assigns) do
     ~F"""
     <Layout current_user={@current_user} flashes={@flash}>
-      Denizen Profile Page for {@user.handle}
-      <div>
-        <p>Name: {@user.name}</p>
-        <p>Bio: {@user.bio}</p>
-      </div>
-      {#if @current_user && @current_user.id == @user.id}
-        <LiveRedirect label="Edit" to={Routes.denizen_edit_path(Endpoint, :edit, @user.handle)} />
-      {/if}
-      <h2 class="title">Studios</h2>
-      <div class="columns denizen-studios is-multiline">
-        {#for studio <- @studios}
-          <div class="column is-one-quarter">
-            <StudioCard studio={studio} />
+      <section class="hero is-primary">
+        <div class="hero-head">
+
+        </div>
+        <div class="hero-body">
+          <article class="media">
+            <figure class="image is-48x48 media-left">
+              <img src={Routes.static_path(Endpoint, "/images/denizen_default_icon.png")}>
+            </figure>
+            <div class="media-content">
+              <h1 class = "title"> {@user.handle} </h1>
+              <p class = "subtitle"> Witty phrase here. </p>
+            </div>
+            <div class="media-right">
+              <a class="button is-warning">Follow</a>
+            </div>
+          </article>
+
+        </div>
+        <div class="hero-foot">
+
+          <nav class="tabs is-boxed is-right">
+            <div class="container">
+              <ul>
+                <li class="is-active">
+                  <a> Profile Home </a>
+                </li>
+                <li> <a>Featured</a> </li>
+                <li> <a>Characters</a> </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </section>
+
+      <div class="tile is-ancester">
+        <div class="tile is-parent is-12">
+          <div class="tile is-child notification is-success">
+            <h2 class="title">Studios</h2>
+            <div class="columns denizen-studios is-multiline block">
+              {#for studio <- @studios}
+                <div class="column">
+                  <StudioCard studio={studio} />
+                </div>
+              {/for}
+            </div>
           </div>
-        {/for}
+          <div class="tile is-child notification is-warning block">
+            <h2 class="title"> About {@user.handle} </h2>
+            <figure class="image is-3by1" alt="denizen ID">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+            <div class="content">
+              <p class="">Name: {@user.name}</p>
+              <p class="">Bio: {@user.bio}</p>
+                {#if @current_user && @current_user.id == @user.id}
+                  <LiveRedirect label="Edit" to={Routes.denizen_edit_path(Endpoint, :edit, @user.handle)} class="button is-primary is-fullwidth"/>
+                {/if}
+            </div>
+          </div>
+
+        </div>
+        <div class="tile">
+
+        </div>
+
       </div>
+
+
     </Layout>
     """
   end
