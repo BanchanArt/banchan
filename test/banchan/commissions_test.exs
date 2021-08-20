2d11bb89dc543804c74231ecd4f76141cd1e2612
Atrian Wagner <411004+skullbunnygalaxy@users.noreply.github.com>
2021-08-20 17:16:54 -0400

    Cleaning up warnings, adjusting tests, and removing some comments. (#49)
    * Cleaning up warnings, adjusting tests, and removing some comments.
    
    * Cleaning up and updating README.md to include link to wiki.
    
    * Fixed TODO failure with Credo.
    
    Co-authored-by: Atrian Wagner <411004+lunitaire@users.noreply.github.com>

--- test/banchan/commissions_test.exs
+++ test/banchan/commissions_test.exs
@@ -19,54 +19,68 @@ defmodule Banchan.CommissionsTest do
       commission
     end
 
-    # test "list_commissions/0 returns all commissions" do
-    #   commission = commission_fixture()
-    #   assert Commissions.list_commissions() == [commission]
-    # end
+    # adding skip flag for wip test
+    @tag :skip
+    test "list_commissions/0 returns all commissions" do
+      commission = commission_fixture()
+      assert Commissions.list_commissions() == [commission]
+    end
 
-    # test "get_commission!/1 returns the commission with given id" do
-    #   commission = commission_fixture()
-    #   assert Commissions.get_commission!(commission.id) == commission
-    # end
+    # adding skip flag for wip test
+    @tag :skip
+    test "get_commission!/1 returns the commission with given id" do
+      commission = commission_fixture()
+      assert Commissions.get_commission!(commission.id) == commission
+    end
 
-    # test "create_commission/1 with valid data creates a commission" do
-    #   assert {:ok, %Commission{} = commission} = Commissions.create_commission(@valid_attrs)
-    #   assert commission.status == "some status"
-    #   assert commission.title == "some title"
-    # end
+    # adding skip flag for wip test
+    @tag :skip
+    test "create_commission/1 with valid data creates a commission" do
+      assert {:ok, %Commission{} = commission} = Commissions.create_commission(@valid_attrs)
+      assert commission.status == "some status"
+      assert commission.title == "some title"
+    end
 
     test "create_commission/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Commissions.create_commission(@invalid_attrs)
     end
 
-    # test "update_commission/2 with valid data updates the commission" do
-    #   commission = commission_fixture()
+    # adding skip flag for wip test
+    @tag :skip
+    test "update_commission/2 with valid data updates the commission" do
+      commission = commission_fixture()
 
-    #   assert {:ok, %Commission{} = commission} =
-    #            Commissions.update_commission(commission, @update_attrs)
+      assert {:ok, %Commission{} = commission} =
+               Commissions.update_commission(commission, @update_attrs)
 
-    #   assert commission.status == "some updated status"
-    #   assert commission.title == "some updated title"
-    # end
+      assert commission.status == "some updated status"
+      assert commission.title == "some updated title"
+    end
 
-    # test "update_commission/2 with invalid data returns error changeset" do
-    #   commission = commission_fixture()
+    # adding skip flag for wip test
+    @tag :skip
+    test "update_commission/2 with invalid data returns error changeset" do
+      commission = commission_fixture()
 
-    #   assert {:error, %Ecto.Changeset{}} =
-    #            Commissions.update_commission(commission, @invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} =
+               Commissions.update_commission(commission, @invalid_attrs)
 
-    #   assert commission == Commissions.get_commission!(commission.id)
-    # end
+      assert commission == Commissions.get_commission!(commission.id)
+    end
 
-    # test "delete_commission/1 deletes the commission" do
-    #   commission = commission_fixture()
-    #   assert {:ok, %Commission{}} = Commissions.delete_commission(commission)
-    #   assert_raise Ecto.NoResultsError, fn -> Commissions.get_commission!(commission.id) end
-    # end
+    # adding skip flag for wip test
+    @tag :skip
+    test "delete_commission/1 deletes the commission" do
+      commission = commission_fixture()
+      assert {:ok, %Commission{}} = Commissions.delete_commission(commission)
+      assert_raise Ecto.NoResultsError, fn -> Commissions.get_commission!(commission.id) end
+    end
 
-    # test "change_commission/1 returns a commission changeset" do
-    #   commission = commission_fixture()
-    #   assert %Ecto.Changeset{} = Commissions.change_commission(commission)
-    # end
+    # adding skip flag for wip test
+    @tag :skip
+    test "change_commission/1 returns a commission changeset" do
+      commission = commission_fixture()
+      assert %Ecto.Changeset{} = Commissions.change_commission(commission)
+    end
   end
 end
