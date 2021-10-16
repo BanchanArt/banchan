159826b9ac5c32e29ce9cb6e107208f4f26322bc
Kat Marchán <kzm@zkat.tech>
2021-10-15 19:50:07 -0700

    adding more tailwind plugins

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -37,6 +37,8 @@ module.exports = {
     extend: {},
   },
   plugins: [
+    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
 };
