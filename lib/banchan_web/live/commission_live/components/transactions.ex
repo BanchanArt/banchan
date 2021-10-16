743b996ce37b156d7cb4753c19bf0cb33cdd07b2
Kat Marchán <kzm@zkat.tech>
2021-10-15 21:00:22 -0700

    port commission_live/components/transactions.ex to Tailwind

--- lib/banchan_web/live/commission_live/components/transactions.ex
+++ lib/banchan_web/live/commission_live/components/transactions.ex
@@ -13,11 +13,11 @@ defmodule BanchanWeb.Components.Commissions.Transactions do
         Transactions
       </:header>
 
-      <p><i class="fas fa-donate" /> <span class="tag is-medium is-success">$100.25</span></p>
+      <p><i class="fas fa-donate" /> $100.25</p>
 
       <:footer>
-        <a class="card-footer-item button is-primary" href="#">Request Payment</a>
-        <a class="card-footer-item button is-warning" href="#">Refund</a>
+        <a class="button primary" href="#">Request Payment</a>
+        <a class="button warning" href="#">Refund</a>
       </:footer>
     </Card>
     """
