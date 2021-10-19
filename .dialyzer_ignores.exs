260acbd37295313a4918f37dd255940d80db4e67
Kat Marchán <kzm@zkat.tech>
2021-10-18 17:41:11 -0700

    add unused_fun to dialyzer ignores

--- .dialyzer_ignores.exs
+++ .dialyzer_ignores.exs
@@ -1,5 +1,6 @@
 [
   {":0:unknown_function Function ExUnit.Callbacks.__merge__/3 does not exist."},
   {":0:unknown_function Function ExUnit.CaseTemplate.__proxy__/2 does not exist."},
-  {":0:unknown_function Function Mix.env/0 does not exist."}
+  {":0:unknown_function Function Mix.env/0 does not exist."},
+  {"lib/banchan_web/views/layout_view.ex:1:unused_fun Function render_template/2 will never be called."}
 ]
