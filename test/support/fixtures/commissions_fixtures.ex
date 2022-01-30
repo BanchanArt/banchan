cf578b9abc14883c4be94410492b5474a93faf4c
Kat Marchán <kzm@zkat.tech>
2022-01-29 18:37:13 -0800

    fix fixture

--- test/support/fixtures/commissions_fixtures.ex
+++ test/support/fixtures/commissions_fixtures.ex
@@ -7,14 +7,14 @@ defmodule Banchan.CommissionsFixtures do
   @doc """
   Generate a event.
   """
-  def event_fixture(attrs \\ %{}) do
+  def event_fixture(actor, commission, attrs \\ %{}) do
     {:ok, event} =
       attrs
       |> Enum.into(%{
         data: %{},
         type: "some type"
       })
-      |> Banchan.Commissions.create_event()
+      |> then(&Banchan.Commissions.create_event(actor, commission, &1))
 
     event
   end
