cb683503b98a5945dadae161ca590d5ecb0476db
Kat Marchán <kzm@zkat.tech>
2022-01-30 12:22:09 -0800

    fix event fixture again

--- test/support/fixtures/commissions_fixtures.ex
+++ test/support/fixtures/commissions_fixtures.ex
@@ -7,14 +7,45 @@ defmodule Banchan.CommissionsFixtures do
   @doc """
   Generate a event.
   """
-  def event_fixture(actor, commission, attrs \\ %{}) do
+  def event_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      Banchan.Accounts.register_admin(%{
+        handle: "test-admin",
+        email: "test@example.com",
+        password: "foobarbazquux",
+        password_confirmation: "foobarbazquux"
+      })
+
+    {:ok, studio} =
+      Banchan.Studios.new_studio(%Banchan.Studios.Studio{artists: [user]}, %{
+        handle: "test-studio",
+        name: "Test Studio",
+        description: "stuff for testing"
+      })
+
+    {:ok, offering} =
+      Banchan.Offerings.new_offering(studio, %{
+        type: "illustration",
+        index: 0,
+        name: "Illustration",
+        description: "A detailed illustration with full rendering and background.",
+        open: true
+      })
+
+    {:ok, commission} =
+      Banchan.Commissions.create_commission(user, offering, %{
+        title: "Please do this thing",
+        description: "the thing to do is like so",
+        tos_ok: true
+      })
+
     {:ok, event} =
       attrs
       |> Enum.into(%{
         data: %{},
         type: "some type"
       })
-      |> then(&Banchan.Commissions.create_event(actor, commission, &1))
+      |> then(&Banchan.Commissions.create_event(user, commission, &1))
 
     event
   end
