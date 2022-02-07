3af4c7c2dde3a871462ce504c6fde06097272c3b
Kat Marchán <kzm@zkat.tech>
2022-02-06 18:35:22 -0800

    separate attachments with/without thumbnails

--- lib/banchan_web/live/studio_live/components/comment.ex
+++ lib/banchan_web/live/studio_live/components/comment.ex
@@ -39,36 +39,41 @@ defmodule BanchanWeb.StudioLive.Components.Comment do
         <hr>
         <div class="p-4">
           <ul class="flex space-x-4 p-2">
-            {#for attachment <- @event.attachments}
-              <li class="w-32 h-32">
-                <a
-                  target="_blank"
-                  href={Routes.commission_attachment_path(
+            {#for attachment <- Enum.filter(@event.attachments, & &1.thumbnail)}
+              <li>
+                <img
+                  alt={attachment.upload.name}
+                  title={attachment.upload.name}
+                  class="rounded-box"
+                  src={Routes.commission_attachment_path(
                     Endpoint,
-                    :show,
+                    :thumbnail,
                     @studio.handle,
                     @commission.public_id,
                     attachment.upload.key
                   )}
-                >
-                  {#if attachment.thumbnail}
-                    <img
-                      class="rounded-box"
-                      src={Routes.commission_attachment_path(
-                        Endpoint,
-                        :thumbnail,
-                        @studio.handle,
-                        @commission.public_id,
-                        attachment.upload.key
-                      )}
-                    />
-                  {#else}
-                    {attachment.upload.name}
-                  {/if}
-                </a>
+                />
               </li>
             {/for}
           </ul>
+          <div class="flex flex-col p-2">
+            {#for attachment <- Enum.filter(@event.attachments, &(!&1.thumbnail))}
+              <a
+                target="_blank"
+                href={Routes.commission_attachment_path(
+                  Endpoint,
+                  :show,
+                  @studio.handle,
+                  @commission.public_id,
+                  attachment.upload.key
+                )}
+              >
+                <div title={attachment.upload.name} class="border-2 p-4 m-1">
+                  <i class="float-right fas fa-file-download" /> <p class="truncate">{attachment.upload.name}</p>
+                </div>
+              </a>
+            {/for}
+          </div>
         </div>
       {/if}
     </div>
