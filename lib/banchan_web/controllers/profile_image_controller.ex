a809e943593cdce17ed130f628f0960b639fac59
Kat Marchán <kzm@zkat.tech>
2022-02-09 12:07:11 -0800

    serve defaults or 404 when missing profile images (#155)
    * serve defaults or 404 when missing profile images

--- lib/banchan_web/controllers/profile_image_controller.ex
+++ lib/banchan_web/controllers/profile_image_controller.ex
@@ -8,29 +8,30 @@ defmodule BanchanWeb.ProfileImageController do
   alias Banchan.Uploads
   alias Banchan.Uploads.Upload
 
+  alias BanchanWeb.Endpoint
+
   def pfp(conn, %{"handle" => handle}) do
     user = Accounts.get_user_by_handle!(handle)
 
     conn
-    |> do_upload(user.pfp_img)
+    |> do_upload(user.pfp_img, :pfp_img)
   end
 
   def thumb(conn, %{"handle" => handle}) do
     user = Accounts.get_user_by_handle!(handle)
 
     conn
-    |> do_upload(user.pfp_thumb)
+    |> do_upload(user.pfp_thumb, :pfp_thumb)
   end
 
   def header(conn, %{"handle" => handle}) do
     user = Accounts.get_user_by_handle!(handle)
 
     conn
-    |> do_upload(user.header_img)
+    |> do_upload(user.header_img, :header_img)
   end
 
-  defp do_upload(conn, %Upload{} = upload) do
-    # TODO: upload a "default" image if upload is nil?
+  defp do_upload(conn, %Upload{} = upload, _) do
     conn
     |> put_resp_header("content-length", "#{upload.size}")
     |> put_resp_header(
@@ -50,5 +51,21 @@ defmodule BanchanWeb.ProfileImageController do
         end
       end)
     )
+  end
+
+  defp do_upload(conn, _, :pfp_img) do
+    conn
+    |> redirect(to: Routes.static_path(Endpoint, "/images/denizen_default_icon.png"))
+  end
+
+  defp do_upload(conn, _, :pfp_thumb) do
+    conn
+    |> redirect(to: Routes.static_path(Endpoint, "/images/denizen_default_icon.png"))
+  end
+
+  defp do_upload(conn, _, :header_img) do
+    conn
+    |> resp(404, "")
+    |> send_resp()
   end
 end
