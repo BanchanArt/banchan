6927315c13487f33777d134f989a0727c13edc1f
Kat Marchán <kzm@zkat.tech>
2022-02-11 22:56:37 -0800

    appease credo

--- lib/banchan_web/cache_body_reader.ex
+++ lib/banchan_web/cache_body_reader.ex
@@ -1,4 +1,11 @@
 defmodule BanchanWeb.CacheBodyReader do
+  @moduledoc """
+  Caches the body of a request in the conn.assigns.
+
+  Used for making sure, for example, that we have access to the raw JSON string for JSON requests.
+  """
+
+  # TODO: Is this really what we want to do to enable request verification for Stripe?
   def read_body(conn, opts) do
     {:ok, body, conn} = Plug.Conn.read_body(conn, opts)
     conn = update_in(conn.assigns[:raw_body], &[body | &1 || []])
