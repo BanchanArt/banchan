8459523dddc5c0683c91c3ea72dccb4af9a0d805
Kat Marchán <kzm@zkat.tech>
2022-02-20 22:22:35 -0800

    blow away alpine

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 import "phoenix_html"
-import Alpine from "alpinejs"
 import { Socket } from "phoenix"
 import topbar from "topbar"
 import Hooks from "./_hooks"
@@ -7,21 +6,11 @@ import { LiveSocket } from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket('/live', Socket, {
-    dom: {
-        onBeforeElUpdated(from, to) {
-            if (from.__x) {
-                window.Alpine.clone(from.__x, to)
-            }
-        }
-    },
     params: {
         _csrf_token: csrfToken
     },
     hooks: Hooks
 })
-
-window.Alpine = Alpine
-Alpine.start()
 
 document.addEventListener("DOMContentLoaded", () => {
   let theme = localStorage.getItem("theme")
