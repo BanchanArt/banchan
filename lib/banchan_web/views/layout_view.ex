e9148c66b99377a77ba8c75b7f016af21765c3f6
Kat Marchán <kzm@zkat.tech>
2022-06-03 09:04:44 -0700

    favicon time

--- lib/banchan_web/views/layout_view.ex
+++ lib/banchan_web/views/layout_view.ex
@@ -9,6 +9,12 @@ defmodule BanchanWeb.LayoutView do
         <meta charset="utf-8">
         {Phoenix.HTML.Tag.csrf_meta_tag()}
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
         {live_title_tag(assigns[:page_title] || "Banchan Art")}
         <link phx-track-static rel="stylesheet" href={Routes.static_path(@conn, "/assets/app.css")}>
         <script
