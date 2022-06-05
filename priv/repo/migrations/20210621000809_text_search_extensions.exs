78b7d85c6fde09603f4c919ed214625651341e56
Kat Marchán <kzm@zkat.tech>
2022-06-04 20:22:30 -0700

    no fun extensions on gigalixir free tier :(((

--- priv/repo/migrations/20210621000809_text_search_extensions.exs
+++ priv/repo/migrations/20210621000809_text_search_extensions.exs
@@ -2,33 +2,36 @@ defmodule Banchan.Repo.Migrations.TextSearchExtensions do
   use Ecto.Migration
 
   def up do
-    execute """
-    CREATE EXTENSION unaccent;
-    """
+    # TODO: enable these and set stuff up again once we're ready for a paid
+    # gigalixir tier. This isn't supported on the free tier. :(
 
-    execute """
-    CREATE EXTENSION pg_trgm;
-    """
+    # execute """
+    # CREATE EXTENSION unaccent;
+    # """
+
+    # execute """
+    # CREATE EXTENSION pg_trgm;
+    # """
 
     execute """
     CREATE TEXT SEARCH CONFIGURATION banchan_fts ( COPY = english );
     """
 
-    execute """
-    ALTER TEXT SEARCH CONFIGURATION banchan_fts
-      ALTER MAPPING FOR hword, hword_part, word
-      WITH unaccent, simple;
-    """
+    # execute """
+    # ALTER TEXT SEARCH CONFIGURATION banchan_fts
+    #   ALTER MAPPING FOR hword, hword_part, word
+    #   WITH unaccent, simple;
+    # """
   end
 
   def down do
-    execute """
-    DROP EXTENSION unaccent;
-    """
+    # execute """
+    # DROP EXTENSION unaccent;
+    # """
 
-    execute """
-    DROP EXTENSION pg_trgm;
-    """
+    # execute """
+    # DROP EXTENSION pg_trgm;
+    # """
 
     execute """
     DROP TEXT SEARCH CONFIGURATION banchan_fts;
