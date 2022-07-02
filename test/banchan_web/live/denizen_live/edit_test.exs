bb4d2569ed006040a8bee5aa445ffdd53ca7ebf3
Kat Marchán <kzm@zkat.tech>
2022-07-02 11:26:48 -0700

    some new tests

--- test/banchan_web/live/denizen_live/edit_test.exs
+++ test/banchan_web/live/denizen_live/edit_test.exs
@@ -23,6 +23,13 @@ defmodule BanchanWeb.DenizenLive.EditTest do
       assert info.to =~ Routes.login_path(conn, :new)
     end
 
+    test "redirects if logged in as a different non-admin user", %{conn: conn, user: user} do
+      conn = log_in_user(conn, user_fixture())
+      {:error, {:live_redirect, info}} = live(conn, Routes.denizen_edit_path(conn, :edit, user.handle))
+
+      assert info.to =~ Routes.denizen_show_path(conn, :show, user.handle)
+    end
+
     test "preloads current profile values", %{conn: conn, user: user} do
       {:ok, user} =
         Accounts.update_user_profile(
@@ -106,6 +113,54 @@ defmodule BanchanWeb.DenizenLive.EditTest do
 
     test "updates profile values on submit, updates user in db", %{conn: conn, user: user} do
       conn = conn |> log_in_user(user)
+
+      {:ok, page_live, _disconnected_html} =
+        live(conn, Routes.denizen_edit_path(conn, :edit, user.handle))
+
+      page_live
+      |> element(".profile-info")
+      |> render_submit(%{
+        user: %{handle: "newhandle", bio: "new bio", name: "new name", email: "new@email"}
+      })
+
+      assert_redirected(page_live, Routes.denizen_show_path(conn, :show, user.handle))
+
+      db_user = Accounts.get_user!(user.id)
+
+      assert db_user.name == "new name"
+      assert db_user.bio == "new bio"
+
+      # Handle and email cannot be updated through here.
+      assert db_user.email == user.email
+      assert db_user.handle == user.handle
+    end
+
+    test "admins can edit profiles", %{conn: conn, user: user} do
+      conn = conn |> log_in_user(user_fixture(%{roles: [:admin]}))
+
+      {:ok, page_live, _disconnected_html} =
+        live(conn, Routes.denizen_edit_path(conn, :edit, user.handle))
+
+      page_live
+      |> element(".profile-info")
+      |> render_submit(%{
+        user: %{handle: "newhandle", bio: "new bio", name: "new name", email: "new@email"}
+      })
+
+      assert_redirected(page_live, Routes.denizen_show_path(conn, :show, user.handle))
+
+      db_user = Accounts.get_user!(user.id)
+
+      assert db_user.name == "new name"
+      assert db_user.bio == "new bio"
+
+      # Handle and email cannot be updated through here.
+      assert db_user.email == user.email
+      assert db_user.handle == user.handle
+    end
+
+    test "mods can edit profiles", %{conn: conn, user: user} do
+      conn = conn |> log_in_user(user_fixture(%{roles: [:mod]}))
 
       {:ok, page_live, _disconnected_html} =
         live(conn, Routes.denizen_edit_path(conn, :edit, user.handle))
--- test/banchan_web/live/denizen_live/show_test.exs
+++ test/banchan_web/live/denizen_live/show_test.exs
@@ -41,5 +41,64 @@ defmodule BanchanWeb.DenizenLive.ShowTest do
       assert disconnected_html =~ user.name
       assert rendered_html =~ user.name
     end
+
+    test "displays follow button for other logged in users only", %{conn: conn, user: user} do
+      stranger_conn = log_in_user(conn, user_fixture())
+      self_conn = log_in_user(conn, user)
+
+      {:ok, _, html} = live(conn, Routes.denizen_show_path(conn, :show, user.handle))
+      refute html =~ ~r/>\s+Follow\s+</
+
+      {:ok, _, html} = live(stranger_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      assert html =~ ~r/>\s+Follow\s+</
+
+      {:ok, _, html} = live(self_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      refute html =~ ~r/>\s+Follow\s+</
+    end
+
+
+    test "displays edit profile button for self, admins, and mods only", %{conn: conn, user: user} do
+      stranger_conn = log_in_user(conn, user_fixture())
+      admin_conn = log_in_user(conn, user_fixture(%{roles: [:admin]}))
+      mod_conn = log_in_user(conn, user_fixture(%{roles: [:mod]}))
+      self_conn = log_in_user(conn, user)
+
+      {:ok, _, html} = live(conn, Routes.denizen_show_path(conn, :show, user.handle))
+      refute html =~ "Edit Profile"
+
+      {:ok, _, html} = live(stranger_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      refute html =~ "Edit Profile"
+
+      {:ok, _, html} = live(self_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      assert html =~ "Edit Profile"
+
+      {:ok, _, html} = live(mod_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      assert html =~ "Edit Profile"
+
+      {:ok, _, html} = live(admin_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      assert html =~ "Edit Profile"
+    end
+
+    test "displays moderation button for admins and mods only", %{conn: conn, user: user} do
+      stranger_conn = log_in_user(conn, user_fixture())
+      admin_conn = log_in_user(conn, user_fixture(%{roles: [:admin]}))
+      mod_conn = log_in_user(conn, user_fixture(%{roles: [:mod]}))
+      self_conn = log_in_user(conn, user)
+
+      {:ok, _, html} = live(conn, Routes.denizen_show_path(conn, :show, user.handle))
+      refute html =~ "Moderation"
+
+      {:ok, _, html} = live(stranger_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      refute html =~ "Moderation"
+
+      {:ok, _, html} = live(self_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      refute html =~ "Moderation"
+
+      {:ok, _, html} = live(mod_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      assert html =~ "Moderation"
+
+      {:ok, _, html} = live(admin_conn, Routes.denizen_show_path(conn, :show, user.handle))
+      assert html =~ "Moderation"
+    end
   end
 end
