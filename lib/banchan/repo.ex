e952fbc821cb95d1b515a913da907b8600e36a00
Kat Marchán <kzm@zkat.tech>
2022-07-02 17:27:11 -0700

    fmt

--- lib/banchan/repo.ex
+++ lib/banchan/repo.ex
@@ -6,10 +6,14 @@ defmodule Banchan.Repo do
   use Scrivener
 
   def down_all do
-    Ecto.Migrator.run(__MODULE__, Application.app_dir(:banchan, "priv/repo/migrations"), :down, [all: true])
+    Ecto.Migrator.run(__MODULE__, Application.app_dir(:banchan, "priv/repo/migrations"), :down,
+      all: true
+    )
   end
 
   def up_all do
-    Ecto.Migrator.run(__MODULE__, Application.app_dir(:banchan, "priv/repo/migrations"), :up, [all: true])
+    Ecto.Migrator.run(__MODULE__, Application.app_dir(:banchan, "priv/repo/migrations"), :up,
+      all: true
+    )
   end
 end
