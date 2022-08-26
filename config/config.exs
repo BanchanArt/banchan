49b489fec2d7be953af3c5ef87e4e8a1b05d7c08
Kat Marchán <kzm@zkat.tech>
2022-08-25 19:18:36 -0700

    use html5ever for floki and bump floki version

--- config/config.exs
+++ config/config.exs
@@ -72,6 +72,8 @@ config :sentry,
   },
   environment_name: config_env()
 
+config :floki, :html_parser, Floki.HTMLParser.Html5ever
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
