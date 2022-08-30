701093ea1f393ad5c234cee3f3ed26f148069ba3
Kat Marchán <kzm@zkat.tech>
2022-08-29 19:28:52 -0700

    this does not need to be z-50

--- lib/banchan_web/live/studio_live/components/offering_card.ex
+++ lib/banchan_web/live/studio_live/components/offering_card.ex
@@ -53,7 +53,7 @@ defmodule BanchanWeb.StudioLive.Components.OfferingCard do
               end}
             />
           </:image>
-          <div class="z-50 absolute top-4 right-4 flex flex-row flex-wrap gap-2">
+          <div class="absolute top-4 right-4 flex flex-row flex-wrap gap-2">
             {#if @offering.open && !is_nil(@offering.slots)}
               <div class="whitespace-nowrap badge badge-primary">{@available_slots}/{@offering.slots} Slots</div>
             {#elseif !@offering.open && !is_nil(@offering.slots)}
