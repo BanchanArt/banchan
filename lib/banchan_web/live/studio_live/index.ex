feb06afe37fe2ceea6536f319bba6c7e461caeab
LuxBrush <82244625+LuxBrush@users.noreply.github.com>
2022-08-30 20:10:19 -0700

    Updating the plus button in studio index page to be centered. (#362)
    If I have the tailwinds css right. This should center the plus mark in the button.
    The resulting output code should have a margin of auto around the span with before and after elements that have a display of table and negative margins.

--- lib/banchan_web/live/studio_live/index.ex
+++ lib/banchan_web/live/studio_live/index.ex
@@ -51,7 +51,7 @@ defmodule BanchanWeb.StudioLive.Index do
         {/for}
         <LiveRedirect to={Routes.studio_new_path(Endpoint, :new)}>
           <Card class="border-2 border-dashed shadow-xs opacity-50 hover:opacity-100 hover:bg-base-200 h-full transition-all">
-            <span class="text-6xl mx-auto my-auto flex items-center justify-center h-full">+</span>
+            <span class="text-6xl m-auto before:-mb-6 before:table after:-mt-2 after:table">+</span>
           </Card>
         </LiveRedirect>
       </div>
