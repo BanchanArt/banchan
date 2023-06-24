24f3a353bdb5a424932a9c8b33c194e7c81ef874
Kat Marchán <kzm@zkat.tech>
2023-06-24 15:46:07 -0700

    convert Avatar component to use `~p` routes

--- lib/banchan_web/components/avatar.ex
+++ lib/banchan_web/components/avatar.ex
@@ -24,20 +24,20 @@ defmodule BanchanWeb.Components.Avatar do
         {#if !Accounts.active_user?(@user)}
           <div class="w-full h-full bg-neutral-focus" />
         {#elseif @link}
-          <LiveRedirect to={Routes.denizen_show_path(Endpoint, :show, @user.handle)}>
+          <LiveRedirect to={~p"/denizens/#{@user.handle}"}>
             {#if @thumb && @user.pfp_thumb_id}
-              <img src={Routes.public_image_path(Endpoint, :image, :user_pfp_thumb, @user.pfp_thumb_id)}>
+              <img src={~p"/images/user_pfp_thumb/#{@user.pfp_thumb_id}"}>
             {#elseif !@thumb && @user.pfp_img_id}
-              <img src={Routes.public_image_path(Endpoint, :image, :user_pfp_img, @user.pfp_img_id)}>
+              <img src={~p"/images/user_pfp_img/#{@user.pfp_img_id}"}>
             {#else}
-              <img src={Routes.static_path(Endpoint, "/images/denizen_default_icon.png")}>
+              <img src={~p"/images/denizen_default_icon.png"}>
             {/if}
           </LiveRedirect>
         {#else}
           {#if @user.pfp_thumb_id}
-            <img src={Routes.public_image_path(Endpoint, :image, :user_pfp_thumb, @user.pfp_thumb_id)}>
+            <img src={~p"/images/user_pfp_thumb/#{@user.pfp_thumb_id}"}>
           {#else}
-            <img src={Routes.static_path(Endpoint, "/images/denizen_default_icon.png")}>
+            <img src={~p"/images/denizen_default_icon.png"}>
           {/if}
         {/if}
       </div>
