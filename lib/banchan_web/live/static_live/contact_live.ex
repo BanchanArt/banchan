029f93efdfa8a05c50f8483786576cf605472f6d
Kat Marchán <kzm@zkat.tech>
2023-07-07 18:38:05 -0700

    Filled out Contact Us page

--- lib/banchan_web/live/static_live/contact_live.ex
+++ lib/banchan_web/live/static_live/contact_live.ex
@@ -15,7 +15,31 @@ defmodule BanchanWeb.StaticLive.Contact do
       <#Markdown class="prose">
         # Contact Us
 
-        Feel free to email us at [support@banchan.art](mailto:support@banchan.art).
+        ## General
+
+        For general support issues, we can be reached at [support@banchan.art](mailto:support@banchan.art).
+        We're a very small team, so please give us some time to respond.
+
+        We're also available [on our Discord](https://discord.gg/FUkTHjGKJF), where
+        most of the project discussion happens. Come chat with us! We're all stronger together.
+
+        We also have [a Twitter account](https://twitter.com/banchan_art)
+        and [Mastodon/Fediverse account](https://mastodon.art/@banchan).
+
+        ## Security
+
+        Please direct any security-relared emails at [security@banchan.art](mailto:security@banchan.art) and
+        we'll handle it as soon as possible.
+
+        ## Address
+
+        Our mailing address is:
+
+        Banchan Art LLC
+
+        440 N Barranca Ave #8687
+
+        Covina, CA 91723
       </#Markdown>
     </Layout>
     """
