b88154bbd08708d5cf2f50773b9ccccae17d16de
Kat Marchán <kzm@zkat.tech>
2023-07-07 22:25:34 -0700

    filled out About Us page

--- lib/banchan_web/live/static_live/about_us_live.ex
+++ lib/banchan_web/live/static_live/about_us_live.ex
@@ -16,8 +16,19 @@ defmodule BanchanWeb.StaticLive.AboutUs do
         # About Banchan Art
 
         We are a [Platform Co-operative](https://en.wikipedia.org/wiki/Platform_cooperative)
-        of workers and artists coming together to create a new art community where artists
-        can grow and do their work safely.
+        of workers and artists coming together to create a new kind of art community.
+
+        ## Our Mission
+
+        Our mission is to build an alternative space to the traditional, often funded by
+        [venture capitalist](https://en.wikipedia.org/wiki/Venture_capital), which are built
+        to extract value from artists and their work, at their expense, while doing their best
+        to crush any alternative they might otherwise have.
+
+        We work in solidarity with organizations like [Artisans.Coop](https://artisans.coop/),
+        [Comradery](https://comradery.co/), and [Ampled](https://www.ampled.com/) to imagine
+        a different economy for the future.
+
       </#Markdown>
     </Layout>
     """
