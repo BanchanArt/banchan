f0bcf0446b7f625233033bdbf78c404f54881478
Kat Marchán <kzm@zkat.tech>
2023-07-13 17:20:59 -0700

    404 for pending images instead of crashing

--- lib/banchan_web/controllers/public_image_controller.ex
+++ lib/banchan_web/controllers/public_image_controller.ex
@@ -12,46 +12,56 @@ defmodule BanchanWeb.PublicImageController do
   def download(conn, %{"id" => upload_id, "type" => type}) do
     upload = get_by_type!(type, upload_id)
 
-    conn
-    |> put_resp_header("content-length", "#{upload.size}")
-    |> put_resp_header(
-      "content-disposition",
-      "attachment; filename=\"#{upload.name || upload.key}\""
-    )
-    |> put_resp_content_type(upload.type)
-    |> send_chunked(200)
-    |> then(
-      &Enum.reduce_while(Uploads.stream_data!(upload), &1, fn chunk, conn ->
-        case chunk(conn, chunk) do
-          {:ok, conn} ->
-            {:cont, conn}
+    if upload.pending do
+      conn
+      |> send_resp(404, "Image is pending processing")
+    else
+      conn
+      |> put_resp_header("content-length", "#{upload.size}")
+      |> put_resp_header(
+        "content-disposition",
+        "attachment; filename=\"#{upload.name || upload.key}\""
+      )
+      |> put_resp_content_type(upload.type)
+      |> send_chunked(200)
+      |> then(
+        &Enum.reduce_while(Uploads.stream_data!(upload), &1, fn chunk, conn ->
+          case chunk(conn, chunk) do
+            {:ok, conn} ->
+              {:cont, conn}
 
-          {:error, :closed} ->
-            {:halt, conn}
-        end
-      end)
-    )
+            {:error, :closed} ->
+              {:halt, conn}
+          end
+        end)
+      )
+    end
   end
 
   def image(conn, %{"id" => upload_id, "type" => type}) do
     upload = get_by_type!(type, upload_id)
 
-    conn
-    |> put_resp_header("content-length", "#{upload.size}")
-    |> put_resp_header("cache-control", "max-age=604800, must-revalidate")
-    |> put_resp_content_type(upload.type)
-    |> send_chunked(200)
-    |> then(
-      &Enum.reduce_while(Uploads.stream_data!(upload), &1, fn chunk, conn ->
-        case chunk(conn, chunk) do
-          {:ok, conn} ->
-            {:cont, conn}
+    if upload.pending do
+      conn
+      |> send_resp(404, "Image is pending processing")
+    else
+      conn
+      |> put_resp_header("content-length", "#{upload.size}")
+      |> put_resp_header("cache-control", "max-age=604800, must-revalidate")
+      |> put_resp_content_type(upload.type)
+      |> send_chunked(200)
+      |> then(
+        &Enum.reduce_while(Uploads.stream_data!(upload), &1, fn chunk, conn ->
+          case chunk(conn, chunk) do
+            {:ok, conn} ->
+              {:cont, conn}
 
-          {:error, :closed} ->
-            {:halt, conn}
-        end
-      end)
-    )
+            {:error, :closed} ->
+              {:halt, conn}
+          end
+        end)
+      )
+    end
   end
 
   defp get_by_type!(type, upload_id)
