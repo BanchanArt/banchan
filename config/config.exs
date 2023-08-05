b3e13f383b8c4ab70f166a4ca7834567ecb2a7ae
Kat Marchán <kzm@zkat.tech>
2023-08-05 13:20:49 -0700

    bump target es version

--- config/config.exs
+++ config/config.exs
@@ -47,7 +47,7 @@ config :esbuild,
   version: "0.12.18",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets --external:/images --external:favicon.ico --external:robots.txt --external:/.well-known),
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/images --external:favicon.ico --external:robots.txt --external:/.well-known),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ],
