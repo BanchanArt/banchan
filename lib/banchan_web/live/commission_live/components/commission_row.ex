7ebeaa1d3f67bf3711042f276480e4152afdcb4a
Kat Marchán <kzm@zkat.tech>
2023-08-15 19:02:51 -0700

    more details on comm list

--- lib/banchan_web/live/commission_live/components/commission_row.ex
+++ lib/banchan_web/live/commission_live/components/commission_row.ex
@@ -6,7 +6,7 @@ defmodule BanchanWeb.CommissionLive.Components.CommissionRow do
 
   alias Banchan.Commissions.Common
 
-  alias Surface.Components.LivePatch
+  alias Surface.Components.{LivePatch, LiveRedirect}
 
   alias BanchanWeb.Components.{Avatar, Icon, StatusBadge, UserHandle}
 
@@ -39,11 +39,20 @@ defmodule BanchanWeb.CommissionLive.Components.CommissionRow do
     ~F"""
     <li class="relative flex items-center justify-between gap-x-6 p-4 cursor-pointer rounded-box transition-all hover:bg-base-200">
       <div class="min-w-0">
-        <div class="flex items-start gap-x-3">
+        <div class="flex gap-x-3 items-center">
           <LivePatch to={commission_url}>
             <span class="absolute inset-x-0 -top-px bottom-0" />
             <p class="text-md font-semibold leading-6">{@result.commission.title}</p>
           </LivePatch>
+          {#if @result.offering}
+            <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 fill-current">
+              <circle cx="1" cy="1" r="1" />
+            </svg>
+            <LiveRedirect
+              to={~p"/studios/#{@result.studio.handle}/offerings/#{@result.offering.type}"}
+              class="text-sm opacity-75"
+            >{@result.offering.name}</LiveRedirect>
+          {/if}
           <StatusBadge
             class="status"
             label={Common.humanize_status(@result.commission.status)}
@@ -51,13 +60,8 @@ defmodule BanchanWeb.CommissionLive.Components.CommissionRow do
           />
         </div>
         <div class="mt-1 flex flex-wrap items-center gap-x-2 text-xs leading-5">
-          <p class="whitespace-nowrap">Updated <time datetime={@result.updated_at |> Timex.to_datetime() |> Timex.format!("{RFC822}")}>{@result.updated_at |> Timex.to_datetime() |> Timex.format!("{relative}", :relative)}</time></p>
-          <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 fill-current">
-            <circle cx="1" cy="1" r="1" />
-          </svg>
           {#if is_nil(@studio)}
             <div class="self-center inline truncate whitespace-nowrap">
-              By
               <div class="inline font-bold">
                 {#if @result.studio && is_nil(@result.studio.deleted_at)}
                   {@result.studio.name}
@@ -67,7 +71,6 @@ defmodule BanchanWeb.CommissionLive.Components.CommissionRow do
               </div>
             </div>
           {#else}
-            From
             <div class="self-center inline">
               <Avatar link={false} user={@result.client} class="w-2.5" />
             </div>
@@ -75,6 +78,14 @@ defmodule BanchanWeb.CommissionLive.Components.CommissionRow do
               <UserHandle link={false} user={@result.client} />
             </div>
           {/if}
+          <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 fill-current">
+            <circle cx="1" cy="1" r="1" />
+          </svg>
+          <p class="whitespace-nowrap">Submitted <time datetime={@result.commission.inserted_at |> Timex.to_datetime() |> Timex.format!("{RFC822}")}>{@result.commission.inserted_at |> Timex.to_datetime() |> Timex.format!("{relative}", :relative)}</time></p>
+          <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 fill-current">
+            <circle cx="1" cy="1" r="1" />
+          </svg>
+          <p class="whitespace-nowrap">Updated <time datetime={@result.updated_at |> Timex.to_datetime() |> Timex.format!("{RFC822}")}>{@result.updated_at |> Timex.to_datetime() |> Timex.format!("{relative}", :relative)}</time></p>
         </div>
       </div>
       <div class="shrink-0">
