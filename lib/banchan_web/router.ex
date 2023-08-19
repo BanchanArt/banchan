d569b92a1f1574b2e60e60a37ef38ee0a30ad7b7
Kat Marchán <kzm@zkat.tech>
2023-08-18 17:49:19 -0700

    also need this in CSP

--- lib/banchan_web/router.ex
+++ lib/banchan_web/router.ex
@@ -14,7 +14,7 @@ defmodule BanchanWeb.Router do
   @content_security_policy (case Application.compile_env!(:banchan, :env) do
                               :prod ->
                                 "default-src 'self' 'unsafe-eval' 'unsafe-inline' https://plausible.io/js/plausible.outbound-links.js;" <>
-                                  "connect-src wss://#{@host} blob:;" <>
+                                  "connect-src wss://#{@host} blob: https://plausible.io/api/event;" <>
                                   "img-src 'self' blob: data:;" <>
                                   "font-src data:;"
 
