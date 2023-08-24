718ab7dfce7e56ed57689a439e8d7faf1613ce94
rjt-rockx <rjt.rockx@gmail.com>
2023-08-24 21:03:03 +0530

    Fix notification width on mobile (#674)
    * Fix notification layout on mobile
    
    * add tooltip

--- lib/banchan_web/components/notifications.ex
+++ lib/banchan_web/components/notifications.ex
@@ -209,29 +209,31 @@ defmodule BanchanWeb.Components.Notifications do
   def render(assigns) do
     ~F"""
     <div class="relative" :on-click-away="close_menu">
-      <div class="indicator">
-        <button
-          type="button"
-          :on-click="toggle_menu"
-          class={"btn btn-ghost gap-2", "btn-active": @open}
-          aria-label="Notifications"
-        >
-          <Icon name="bell" size="4" />
-          {#if @notifications && @notifications.total_entries > 0}
-            <span class="badge badge-primary">
-              {#if @notifications.total_entries > 99}
-                99+
-              {#else}
-                {@notifications.total_entries}
-              {/if}
-            </span>
-          {/if}
-        </button>
+      <div class="tooltip tooltip-left" data-tip="Notifications">
+        <div class="indicator">
+          <button
+            type="button"
+            :on-click="toggle_menu"
+            class={"btn btn-ghost gap-2", "btn-active": @open}
+            aria-label="Notifications"
+          >
+            <Icon name="bell" size="4" />
+            {#if @notifications && @notifications.total_entries > 0}
+              <span class="badge badge-primary">
+                {#if @notifications.total_entries > 99}
+                  99+
+                {#else}
+                  {@notifications.total_entries}
+                {/if}
+              </span>
+            {/if}
+          </button>
+        </div>
       </div>
       {#if @open}
-        <div class="absolute right-0 z-30 grid max-w-full grid-cols-1 origin-top translate-x-px translate-y-2 border rounded-lg w-96 sm:max-w-sm border-base-content border-opacity-10 bg-base-100 text-base-content">
+        <div class="absolute right-0 z-30 grid max-w-sm grid-cols-1 origin-top translate-x-px translate-y-px border rounded-lg shadow-md w-96 border-base-content border-opacity-10 bg-base-100 text-base-content">
           {#if !@notifications || Enum.empty?(@notifications.entries)}
-            <div class="flex flex-col items-center gap-2 px-4 py-8 mx-auto">
+            <div class="flex flex-col items-center gap-2 px-4 py-10 mx-auto">
               <span class="text-base font-medium text-center text-base-content">
                 No notifications
               </span>
