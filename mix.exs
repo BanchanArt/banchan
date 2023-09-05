fa30fd9b022f61ce9d92418387160d8ba08eddaf
Kat Marchán <kzm@zkat.tech>
2023-09-04 21:54:34 -0700

    Bump version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Banchan.MixProject do
   def project do
     [
       app: :banchan,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [warnings_as_errors: true],
