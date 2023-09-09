a2d35b3b500befa664a4b754c33b9a66bf32bf3d
Kat Marchán <kzm@zkat.tech>
2023-09-08 17:54:26 -0700

    Bump version to 1.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Banchan.MixProject do
   def project do
     [
       app: :banchan,
-      version: "1.2.2",
+      version: "1.2.3",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [warnings_as_errors: true],
