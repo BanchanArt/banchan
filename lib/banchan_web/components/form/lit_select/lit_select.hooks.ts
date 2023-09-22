7925a5e04ff18e9c8a5ca88d7b0aac95d08c2f14
Kat Marchán <kzm@zkat.tech>
2023-09-21 17:10:13 -0700

    more

--- lib/banchan_web/components/form/lit_select/lit_select.hooks.ts
+++ lib/banchan_web/components/form/lit_select/lit_select.hooks.ts
@@ -19,7 +19,6 @@ import { customElement, property, state } from "lit/decorators.js";
 
 // TODO:
 // * highlight multiple things, incl. Shift+arrow management
-// * Show pills for current selections in input box, like Tags does.
 // * Allow combo vs dropdown modes.
 // * slot-based custom styling for option bodies.
 // * Transitions
@@ -70,11 +69,13 @@ export class LitSelectElement extends LitElement {
     super.disconnectedCallback();
   }
 
-  _getOptions() {
+  _getOptions(all = false) {
     return Array.from(this.querySelectorAll("option"))
       .map((option, index) => ({ option, index }))
       .filter(({ option }) => {
-        if (this.filter) {
+        if (all) {
+          return true;
+        } else if (this.filter) {
           return option.innerText
             .toUpperCase()
             .includes(this.filter.toUpperCase());
@@ -114,10 +115,12 @@ export class LitSelectElement extends LitElement {
       } else {
         this.selected.add(index);
       }
-      this.selected = new Set(this.selected);
     } else {
-      this.selected = new Set([index]);
+      this.selected.clear();
+      this.selected.add(index);
     }
+
+    this.requestUpdate();
 
     const option = this.querySelectorAll("option")[index];
     if (option) {
@@ -146,11 +149,10 @@ export class LitSelectElement extends LitElement {
           this.querySelector("select")?.form?.dispatchEvent(
             new Event("submit", { bubbles: true, cancelable: true })
           );
-        }
-        break;
-      case "Space":
-        if (this.highlighted != null) {
-          this._select(this.highlighted);
+        } else {
+          if (this.highlighted != null) {
+            this._select(this.highlighted);
+          }
         }
         break;
       case "ArrowDown":
@@ -170,120 +172,136 @@ export class LitSelectElement extends LitElement {
       return;
     }
     const items = this._getOptions();
-    const max = items[items.length - 1].index;
-    this.highlighted = this.highlighted === max ? 0 : this.highlighted + 1;
+    const max = items[items.length - 1]?.index;
+    if (max == null) {
+      this.highlighted = null;
+      return;
+    }
+    this.highlighted =
+      this.highlighted === max
+        ? items[0]?.index
+        : items.find(({ index }) => index > this.highlighted)?.index ?? 0;
   }
 
   _up() {
     const items = this._getOptions();
-    const max = items[items.length - 1].index;
+    const max = items[items.length - 1]?.index;
+    if (max == null) {
+      this.highlighted = null;
+      return;
+    }
     if (this.highlighted == null) {
       this.highlighted = max;
       return;
     }
-    this.highlighted = this.highlighted === 0 ? max : this.highlighted - 1;
+    this.highlighted =
+      this.highlighted === items[0]?.index
+        ? max
+        : items.findLast(({ index }) => index < this.highlighted)?.index ?? 0;
   }
 
   // Render the UI as a function of component state
   render() {
-    return html`<div @keydown=${this._handleKeydown}>
-      <div class="relative mt-2">
-        <input
-          id="combobox"
-          type="text"
-          class="w-full bg-p-0 input input-bordered focus:ring focus:ring-primary"
-          ,
-          role="combobox"
-          aria-controls="options"
-          aria-expanded="${this.open}"
-          @focus=${() => {
-            this.open = true;
-          }}
-          @click=${() => {
-            this.open = true;
-          }}
-          @input=${(e) => {
-            this.filter = (e.target as HTMLInputElement).value;
-          }}
-        />
-        <button
-          type="button"
-          class="absolute inset-y-0 right-0 h-full rounded-r-md px-2 focus:outline-none"
-          @click=${() => {
-            this.open = !this.open;
-          }}
-        >
-          <div class="flex items-center">
-            <svg
-              class="h-5 w-5 text-base-content"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-        </button>
-        <ul
-          class="relative z-40 mt-1 max-h-60 w-full overflow-auto rounded-md bg-base-100 py-1 text-base shadow-lg ring-1 ring-base-300 ring-opacity-5 focus:outline-none sm:text-sm ${classMap(
-            { hidden: !this.open }
-          )}"
-          id="options"
-          role="listbox"
-        >
-          ${this._getOptions().map(({ option, index }) => {
-            let selected = this.selected?.has(index) ?? false;
-            let highlighted = this.highlighted === index;
+    return html`<div class="relative w-full bg-p-0">
+      <ul
+        class="flex flex-row flex-wrap px-2 gap-2 border focus-within:ring ring-primary border-base-content border-opacity-20 bg-base-100 rounded-btn cursor-text items-center input input-bordered"
+        role="combobox"
+        aria-controls="options"
+        aria-expanded="${this.open}"
+        @focus=${() => {
+          this.open = true;
+        }}
+        @click=${() => {
+          this.open = true;
+          this.shadowRoot?.querySelector("input")?.focus();
+        }}
+      >
+        ${this._getOptions(true)
+          .filter(({ index }) => this.selected.has(index))
+          .map(({ option, index }) => {
             return html`
               <li
-                class="relative cursor-default select-none py-2 pl-3 pr-9 text-base-content ${classMap(
-                  {
-                    "text-primary-content": highlighted,
-                    "bg-primary": highlighted,
-                  }
-                )}"
-                id="option-${index}"
-                role="option"
-                tabindex="-1"
-                aria-selected="${highlighted}"
-                aria-checked="${selected}"
-                aria-label=${option.innerText}
-                @mouseenter=${() => {
-                  this.highlighted = index;
-                }}
-                @click=${() => this._select(index)}
+                class="flex flex-row items-center max-w-full gap-1 px-2 py-1 text-xs font-semibold text-opacity-75 no-underline uppercase rounded-full cursor-default bg-opacity-10 h-fit w-fit hover:bg-opacity-20 active:bg-opacity-20 border-base-content border-opacity-10 hover:text-opacity-100 active:text-opacity-100 bg-base-content"
               >
-                <span
-                  class="truncate ${classMap({
-                    "font-semibold": selected,
-                  })}"
-                  >${option.innerText}</span
+                <span class="tracking-wide">${option.innerText}</span>
+                <button
+                  type="button"
+                  class="text-xs opacity-50 cursor-pointer hover:opacity-100 active:opacity-100"
+                  aria-controls="options"
+                  aria-label="Remove ${option.innerText}"
+                  @click=${() => this._select(index)}
                 >
-                <span
-                  class="absolute inset-y-0 right-0 pr-4 ${classMap({
-                    hidden: !selected,
-                    "text-primary": !highlighted,
-                    "text-primary-content": highlighted,
-                  })}"
-                  aria-hidden="${!selected}"
-                >
-                  <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path
-                      fill-rule="evenodd"
-                      d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </span>
+                  ✕
+                </button>
               </li>
             `;
           })}
-        </ul>
-      </div>
+        <li>
+          <input
+            type="text"
+            class="w-full h-full px-0 overflow-visible border-transparent border-none shadow-none input-field bg-base-100 input-sm focus:outline-none focus:border-none focus:border-transparent focus:ring-0 focus:ring-transparent"
+            @input=${(e) => {
+              this.filter = (e.target as HTMLInputElement).value;
+            }}
+            @keydown=${this._handleKeydown}
+          />
+        </li>
+      </ul>
+      <ul
+        class="relative z-40 mt-1 max-h-60 w-full overflow-auto rounded-md bg-base-100 py-1 text-base shadow-lg ring-1 ring-base-300 ring-opacity-5 focus:outline-none sm:text-sm ${classMap(
+          { hidden: !this.open }
+        )}"
+        id="options"
+        role="listbox"
+      >
+        ${this._getOptions().map(({ option, index }) => {
+          let selected = this.selected?.has(index) ?? false;
+          let highlighted = this.highlighted === index;
+          return html`
+            <li
+              class="relative cursor-default select-none py-2 pl-3 pr-9 text-base-content ${classMap(
+                {
+                  "text-primary-content": highlighted,
+                  "bg-primary": highlighted,
+                }
+              )}"
+              id="option-${index}"
+              role="option"
+              tabindex="-1"
+              aria-selected="${highlighted}"
+              aria-checked="${selected}"
+              aria-label=${option.innerText}
+              @mouseenter=${() => {
+                this.highlighted = index;
+              }}
+              @click=${() => this._select(index)}
+            >
+              <span
+                class="truncate ${classMap({
+                  "font-semibold": selected,
+                })}"
+                >${option.innerText}</span
+              >
+              <span
+                class="absolute inset-y-0 right-0 pr-4 ${classMap({
+                  hidden: !selected,
+                  "text-primary": !highlighted,
+                  "text-primary-content": highlighted,
+                })}"
+                aria-hidden="${!selected}"
+              >
+                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+            </li>
+          `;
+        })}
+      </ul>
     </div> `;
   }
 }
