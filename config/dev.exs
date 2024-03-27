c9fdf95c7d21aafc23e6bf7c0d3bc3165307c572
Kat Marchán <kzm@zkat.tech>
2024-03-26 17:24:49 -0700

    fix regex

--- config/dev.exs
+++ config/dev.exs
@@ -59,7 +59,7 @@ config :banchan, BanchanWeb.Endpoint,
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"priv/posts/*/.*(md)$",
+      ~r"priv/posts/.*(md)$",
       ~r"lib/banchan_web/(live|views|components)/.*(ex|js|ts)$",
       ~r"lib/banchan_web/templates/.*(eex)$",
       ~r"priv/catalogue/.*(ex)$"
